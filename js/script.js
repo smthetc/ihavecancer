5d6f57f6693237892f9834b6ff91cb948f6d0dcd
ZaBralow <pavlik.elsukov@mail.ru>
2018-10-16 17:39:19 +0300

    commits have cancer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,93 @@
-var rooffloor = ("___________");
-var wall = ("|4444|3333|");
-var center=("|----+----|");
+/* console.log("budl");
+$(document).ready(function(){
+
+	$(".block1").click(function(){
+		$(".block1").animate({
+			padding:"20",
+			width:"100px",
+		},1000); 
+		
+	});
+
+});
 
 
-console.log(rooffloor);
-console.log(wall);
-console.log(wall);
-console.log(center);
-console.log(wall);
-console.log(wall);
-console.log(rooffloor);
+$(document).ready(function(){
+
+
+	$("#idid").click(function(){
+		$("#block1").toggle(3000);
+
+	});
+	$("#caca").add("p").css("background-color", "blue");
+      $("#re").click(function(){
+      $("#er").after("<p>Я второй абзац.</p>");
+   });
+      
+      
+  });
+*/
+
+
+/* var gg = function (g){
+	console.log(g);
+}
+
+
+var coc = {};
+
+coc.rabotaet = "rabota";
+
+coc.gotovit = "eda";
+
+coc.delaet = function(){
+	return "eda_gotovitsya";
+}
+
+gg(coc.delaet());
+
+gg(coc.rabotaet );
+gg(coc.gotovit );
+
+
+var gg = {
+
+	"ship":"30",
+	"door":"str400",
+	"mymethod": function(){
+		console.log("eda_gotovitsya")
+	}
+};
+*/
+
+
+ var view = function (g){
+	console.log(g);
+}
+
+
+var coc_gog = {};
+
+coc_gog.obj = {};
+
+coc_gog.obj.razmer = 'lel';
+coc_gog.obj.gradus = 'lol';
+view(coc_gog.obj.razmer);
+
+
+
+
+
+
+
+var ququ = document.getElementById("kek");
+
+
+
+
+var kuku = ququ.getElementsByTagName("p");
+
+
+view(kuku);
+
+
